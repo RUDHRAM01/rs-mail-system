30594ba997eb5bfac00a5fa3ed87249ae68bd3c6
RUDHRAM01 <rudhramsaraswat2770@gmail.com>
2023-01-21 21:09:13 +0530

    update

--- server.js
+++ server.js
@@ -4,10 +4,11 @@ const cors = require("cors");
 const nodemailer = require("nodemailer");
 
 const app = express();
+const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 app.use("/", router);
-app.listen(3000, () => console.log("Server Running"));
+app.listen(port, () => console.log("Server Running, ",port));
 
 
 const contactEmail = nodemailer.createTransport({
